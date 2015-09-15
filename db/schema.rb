56ca967ebbfead6440dcd33d3ec251f1c468bce0
Jake Park <jake.park@caa.columbia.edu>
2015-09-15 10:20:36 -0400

    update user.rb model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915011736) do
+ActiveRecord::Schema.define(version: 20150915135948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,5 @@ ActiveRecord::Schema.define(version: 20150915011736) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
 end
