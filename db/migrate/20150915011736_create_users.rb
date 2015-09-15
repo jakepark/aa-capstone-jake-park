9fea69be1cafb439461d205ea1a70bf6b013dfdf
Jake Park <jake.park@caa.columbia.edu>
2015-09-15 09:56:59 -0400

    update create users migration.

--- db/migrate/20150915011736_create_users.rb
+++ db/migrate/20150915011736_create_users.rb
@@ -1,16 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :id, null: false
+      t.string :email, null: false
       t.string :user_fname, null: false
       t.string :user_lname, null: false
-      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
-      t.text :avatar_url, null: false
-      t.string :birth_date, null: false
-      t.string :birth_year, null: false
-      t.string :gender, null: false
+      t.text :avatar_url
+      t.string :birth_date
+      t.string :birth_year
+      t.string :gender
 
       t.timestamps
     end
