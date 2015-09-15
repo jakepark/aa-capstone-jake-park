4b008adbcdd0d962894e76a821e29c3a8442114b
Jake Park <jake.park@caa.columbia.edu>
2015-09-15 10:09:59 -0400

    rails g User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+# Schema
+# create_table "users", force: :cascade do |t|
+#   t.string   "email",           null: false
+#   t.string   "user_fname",      null: false
+#   t.string   "user_lname",      null: false
+#   t.string   "password_digest", null: false
+#   t.string   "session_token",   null: false
+#   t.text     "avatar_url"
+#   t.string   "birth_date"
+#   t.string   "birth_year"
+#   t.string   "gender"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+#
+#   add_index :users, :session_token, :unique => true
+#   add_index :users, :email, :unique => true
+
 class User < ActiveRecord::Base
-  
+  validates :email, :user_fname, :user_lname, :session_token, presence: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+  validates :email, uniqueness: true
+
 end
--- db/migrate/20150915135948_create_users.rb
+++ db/migrate/20150915135948_create_users.rb
@@ -14,7 +14,6 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :session_token, :unique => true
     add_index :users, :email, :unique => true
   end
 end
