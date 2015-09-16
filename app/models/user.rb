b732978916bf3e08c8dfb9e3b5d2096e85206d64
Jake Park <jake.park@caa.columbia.edu>
2015-09-16 16:53:58 -0400

    update css for sign on, logged in, index, profile view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,18 +25,16 @@ class User < ActiveRecord::Base
   has_many :passive_friendships,
     class_name: "Friendship",
     foreign_key: :friend_id
-  has_many :active_friends, -> { where(friendships: {approved: true})},
-    through: :friendships,
-    source: :friend
-  has_many :passive_friends, -> { where(friendships: {approved: true})},
-    through: :passive_friendships,
-    source: :user
-  has-many :pending_friends, -> { where(friendships: {approved: false})},
-    through: :friendships,
-    source: :friend
-  has_many :requested_friendships, -> {where(friendships: {approved: false})},
-    through: :passive_friendships,
-    source: :user
+
+  has_many :active_friends, -> { where(friendships: { approved: true}) },
+    :through => :friendships, :source => :friend
+  has_many :passive_friends, -> { where(friendships: { approved: true}) },
+    :through => :passive_friendships, :source => :user
+  has_many :pending_friends, -> { where(friendships: { approved: false}) },
+    :through => :friendships, :source => :friend
+  has_many :requested_friendships, -> { where(friendships: { approved: false}) },
+    :through => :passive_friendships, :source => :user
+
 
 
   attr_reader :password
