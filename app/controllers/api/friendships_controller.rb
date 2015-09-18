92eca456753ba757f82c8f2650350d8d8e173206
Jake Park <jake.park@caa.columbia.edu>
2015-09-18 12:39:49 -0400

    start friendships.

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,22 +1,67 @@
 class Api::FriendshipsController < ApplicationController
-  def show
-    @friendship = Friendship.find(friendship_params[:id])
+  before_action :set_friendship, only: [:show, :edit, :update, :destroy]
 
-    if @friendship
-      render :show
+  # GET /friendships
+  # GET /friendships.json
+  def index
+    @friendships = Friendship.all
+  end
+
+  # GET /friendships/1
+  # GET /friendships/1.json
+  def show
+  end
+
+  # GET /friendships/new
+  def new
+    @friendship = Friendship.new
+  end
+
+  # GET /friendships/1/edit
+  def edit
+  end
+
+  # POST /friendships
+  # POST /friendships.json
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id], approved: "false")
+    if @friendship.save
+      flash[:notice] = "Friend requested."
+      redirect_to :back
     else
-      render json: ["User doesn't exist."], status: 422
+      flash[:error] = "Unable to request friendship."
+      redirect_to :back
     end
   end
 
-  def index
-    @friendships = Friendship.all
-        render json: @users
+  # PATCH/PUT /friendships/1
+  # PATCH/PUT /friendships/1.json
+  def update
+  @friendship = Friendship.where(friend_id: current_user, user_id: params[:id]).first
+  @friendship.update(approved: true)
+    if @friendship.save
+      redirect_to root_url, :notice => "Successfully confirmed friend!"
+    else
+      redirect_to root_url, :notice => "Sorry! Could not confirm friend!"
+    end
   end
 
+  # DELETE /friendships/1
+  # DELETE /friendships/1.json
+  def destroy
+    @friendship = Friendship.where(friend_id: [current_user, params[:id]]).where(user_id: [current_user, params[:id]]).last
+    @friendship.destroy
+    flash[:notice] = "Removed friendship."
+    redirect_to :back
+  end
 
   private
-    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friendship
+      @friendship = Friendship.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def friendship_params
       params.require(:friendship).permit(:user_id, :friend_id, :approved)
     end
