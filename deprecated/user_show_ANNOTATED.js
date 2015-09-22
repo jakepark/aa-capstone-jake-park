1591cac47bc48ad37b32df8ed87ff398a0123330
Jake Park <jake.park@caa.columbia.edu>
2015-09-22 18:09:09 -0400

    Add, Remove, Approve, Deny buttons added. Not all functionality tested complete yet.

--- deprecated/user_show_ANNOTATED.js
+++ deprecated/user_show_ANNOTATED.js
@@ -30,49 +30,62 @@ myfacebook.Views.UserShow = Backbone.View.extend({
     // carl: this.model
     // this.model.requests() // will have dug's request.
 
+    // case a: alpha has requested dug's friendship.
+
+    var a = this.model.friendships().findWhere({
+      user_id: this.model.get('id'),
+      friend_id: myfacebook.currentUser.get('id')
+    })
+
+    if (a) { this.$el.prepend(
+      "<div class='approve_friend'><button>Approve Friend</button></div><div class='deny_friend'><button>Deny Friend</button></div>"
+    )};
+
+    // case b: dug and target have no friendship status. add friend.
+
+    var b = true
+
+    if (b) { this.$el.prepend("<div class='request_friend'><button>Add Friend</button></div>")}
+
+
+    // case c: dug has already requested carl's friendship.
+    debugger
+    var c = this.model.requests().findWhere({
+      id: myfacebook.currentUser.get('id')
+    })
+
+    if (c) {
+      // do nothing. seriously.
+    }
+
     // case d: dug and target are friends. delete friend.
 
     var d = this.model.friends().findWhere({
       id: myfacebook.currentUser.get('id')
-    });
-
-    if (this.model.get('id') !== myfacebook.currentUser.get('id')) {
+    })
 
     if (d) { this.$el.prepend(
       "<div class='remove_friend'><button>Remove Friend</button></div>"
-    )} else {
+    )}
+
+   //
+  //   &&
+  //  (this.model.friends().findWhere({
+  //    user_id: myfacebook.currentUser.get('id'),
+  //    friend_id: this.model.get('id')
+  //  }) === undefined)
+  //  &&
+  //  (myfacebook.currentUser.friends().findWhere({
+  //    user_id: this.model.get('id'),
+  //    friend_id: myfacebook.currentUser.get('id')
+  //  }) === undefined)
+  //  &&
+  //  (myfacebook.currentUser.friends().findWhere({
+  //    user_id: myfacebook.currentUser.get('id'),
+  //    friend_id: this.model.get('id')
+  //  }) === undefined)
 
 
-      // case c: dug has already requested carl's friendship.
-
-      var c = this.model.requests().findWhere({
-        id: myfacebook.currentUser.get('id')
-      });
-
-      if (c) {
-
-        // do nothing. seriously.
-
-      } else {
-
-          // case a: alpha has requested dug's friendship.
-
-          var a = this.model.friendships().findWhere({
-            user_id: this.model.get('id'),
-            friend_id: myfacebook.currentUser.get('id')
-          });
-
-          if (a) { this.$el.prepend(
-            "<div class='approve_friend'><button>Approve Friend</button></div><div class='deny_friend'><button>Deny Friend</button></div>"
-          )} else {
-
-          // case b: dug and target have no friendship status. add friend.
-
-            this.$el.prepend("<div class='request_friend'><button>Add Friend</button></div>")
-          }
-        }
-      }
-    }
 
     return this;
   },
