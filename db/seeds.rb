dfd1713da5684882452301141593fe0e146999bb
Jake Park <jake.park@caa.columbia.edu>
2015-09-24 09:19:11 -0400

    Add posts to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,45 @@ u5 = User.create(email: 'ellie@mail.com',
   birth_month: 'Feb', birth_day: '14', birth_year: '1936',
   gender: 'f')
 
-u7 = User.create(email: 'russell@mail.com',
+u6 = User.create(email: 'russell@mail.com',
   name_first: 'russell', name_last: 'nagai', password: 'password',
-  birth_month: 'Oct', birth_day: '17', birth_year: '1985',
+  birth_month: 'Oct', birth_day: '17', birth_year: '2007',
   gender: 'm')
+
+# create_table "posts", force: :cascade do |t|
+#   t.text     "body",                     null: false
+#   t.integer  "user_id",                  null: false
+#   t.float    "ord",        default: 0.0
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+
+# oh ruby, it needs bind and sendthis method..
+# idx = 1
+# posts = []
+# users = ["Dug", "Russell", "Carl"]
+# while idx <= 3
+#   idx_u = 0
+#   while idx_u < 3
+#     post = Post.create(user_id: idx, body: "Post #{idx} from #{users[idx]}")
+#
+#     posts << post
+#     idx_u += 1
+#   end
+#   idx += 1
+#   return posts
+# end
+
+
+p1 = Post.create(user_id: 4, body: 'Post 1 from Dug.')
+p2 = Post.create(user_id: 4, body: 'Post 2 from Dug.')
+p3 = Post.create(user_id: 4, body: 'Post 3 from Dug.')
+p4 = Post.create(user_id: 4, body: 'Post 4 from Dug.')
+
+p5 = Post.create(user_id: 3, body: 'Post 1 from Carl.')
+p6 = Post.create(user_id: 3, body: 'Post 2 from Carl.')
+p7 = Post.create(user_id: 3, body: 'Post 3 from Carl.')
+
+p8 = Post.create(user_id: 6, body: 'Post 1 from Russell.')
+p9 = Post.create(user_id: 6, body: 'Post 2 from Russell.')
+p10 = Post.create(user_id: 6, body: 'Post 3 from Russell.')
